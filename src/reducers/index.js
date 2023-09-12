04478ec7ddfca9a1e1f0cee582a80b3a9ab23b2e
Lindsay Warr <iamalion@gmail.com>
2023-09-12 11:33:01 -0700

    Fix error in initialization

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,10 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
-import { createStore } from 'redux';
-
-let store = createStore(rootReducer);
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainTicketList: ticketListReducer
+  masterTicketList: ticketListReducer
 });
 
 export default rootReducer;
